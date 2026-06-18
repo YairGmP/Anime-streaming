216fa3d239bc13e5071f18abf9ce92005f7fdab5
Desarrollo Software <2302133@utrivieramaya.edu.mx>
2026-06-18 10:46:56 -0500

    Fix: cambiar servidor de embed a gogoplay.to que es funcional

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -45,7 +45,7 @@ const Home = () => {
   }, []);
 
   const handleWatchEpisode = (serie) => {
-    const episodeUrl = `https://anicdn.stream/embed/${serie.title.toLowerCase().replace(/[^\w-]+/g, '-')}-episode-${serie.episode}`;
+    const episodeUrl = `https://gogoplay.to/embed/anime/${serie.title.toLowerCase().replace(/[^\w-]+/g, '-')}-episode-${serie.episode}`;
     const encodedEpisodeUrl = encodeURIComponent(episodeUrl);
     
     // Redirige al componente Episode con los parámetros necesarios
